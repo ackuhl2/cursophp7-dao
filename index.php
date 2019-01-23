4dcb119b78d490a020e037f113fc4902e91e1715
Anderson <ackuhl2@gmail.com>
2019-01-23 21:29:34 -0200

    Criamos Métodos de Lista

--- index.php
+++ index.php
@@ -2,10 +2,24 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//Carrega um usuario
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
 
-$root->loadbyId(3);
+//carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("e");
+//echo json_encode($search);
+
+//carrega uma lista de usuarios usando login e senha
+$usuario = new Usuario();
+$usuario->login("jose", "654321");
+echo $usuario;
+
+
 
 ?>
